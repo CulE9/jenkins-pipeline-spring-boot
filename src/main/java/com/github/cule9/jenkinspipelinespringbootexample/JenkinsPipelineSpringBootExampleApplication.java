309f28acde794a0f359c0a92b13e510b2883e565
CulE <undertaker007@onet.pl>
2020-08-08 14:40:41 +0200

    Initial commit

---
+++ src/main/java/com/github/cule9/jenkinspipelinespringbootexample/JenkinsPipelineSpringBootExampleApplication.java
@@ -0,0 +1,21 @@
+package com.github.cule9.jenkinspipelinespringbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsPipelineSpringBootExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsPipelineSpringBootExampleApplication.class, args);
+    }
+
+    @GetMapping
+    public String hello() {
+        return "Hello World!";
+    }
+
+}
